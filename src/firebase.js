95b8ced7872f2f7a55bd975158a33b99d5375925
YusufKer <yus.ker1994@gmail.com>
2023-03-08 22:00:46 +0200

    adding firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCEDBGqRq8s6ejQPugZ2vN2xBsJUwE-nm0",
+  authDomain: "portfolio-2023-ec21b.firebaseapp.com",
+  projectId: "portfolio-2023-ec21b",
+  storageBucket: "portfolio-2023-ec21b.appspot.com",
+  messagingSenderId: "999042110015",
+  appId: "1:999042110015:web:71bdeb91395878aec89e8a"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
